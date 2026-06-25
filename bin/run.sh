ea8198ca29c2fb594964d382b3168ce4c50b0278
Jared Simpson <jared.simpson@gmail.com>
2026-06-25 16:39:20 -0400

    add moddotplot figures

--- bin/run.sh
+++ bin/run.sh
@@ -27,3 +27,21 @@ if [ ! -f plots/fig1e.pdf ]; then
     cat $D/mHetGla1pri_geneID_by_biotype.txt | cut -f2 | sort | uniq -c | awk '{ print $2 "\t" $1 }' | grep -v biotype >> data/mHetGla1.1.pri.gene_counts.tsv
     Rscript bin/plot_fig_genes.R -g data/mHetGla1.1.pri.gene_counts.tsv -o plots/fig1e.pdf
 fi
+
+if [ ! -f placeholder.pdf ]; then
+    echo "Building ModDotPlots"
+    # this requires https://github.com/jts/ModDotPlotMod installed
+    D=$DROPBOX/Figure_Data/Figure2/Figure2A_StainGlass_Methyl_Cenpa/
+
+    # add 100kb buffer around annotated centromere
+    bedtools slop -b 100000 -i $D/mHetGla1.1.pri_centromere_v20260502.bed -g $D/mHetGla1.1.pri.cleaned.fa.gz.fai > data/mHetGla1.1.pri.centromeres_slop.bed
+
+    # extract
+    bedtools getfasta -fi $D/mHetGla1.1.pri.cleaned.fa.gz -bed data/mHetGla1.1.pri.centromeres_slop.bed > data/mHetGla1.1.pri.centromeres_slop.fa
+
+    # make plots
+    moddotplot static -f data/mHetGla1.1.pri.centromeres_slop.fa \
+                      --cenpa-bw $D/Cenpa_vs_input.log2.utils.bw \
+                      --bedmethyl $D/pup_5mC_merged.cleaned.bed \
+                      -r 250 -o plots/moddotplot
+fi
